c5e693cc717b1dc087b37ab503abc5cc0736f82a
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-04-24 10:30:20 -0600

    cleanup

--- Sample Projects/Rooms/Rooms/ContentView.swift
+++ Sample Projects/Rooms/Rooms/ContentView.swift
@@ -4,17 +4,19 @@ struct ContentView: View {
     var rooms: [Room] = []
     
     var body: some View {
-        List(rooms) { room in
-            Image(room.name)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(8)
-            VStack(alignment: .leading) {
-                Text(room.name)
-                Text("\(room.capacity) people")
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
-            }
+        NavigationView {
+            List(rooms) { room in
+                Image(room.name)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(8)
+                VStack(alignment: .leading) {
+                    Text(room.name)
+                    Text("\(room.capacity) people")
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                }
+            }.navigationBarTitle(Text("Rooms"))
         }
     }
 }
