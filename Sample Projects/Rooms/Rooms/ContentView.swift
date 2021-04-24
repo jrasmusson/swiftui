4a3661951af73690061c0398a533540aee24e156
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-04-24 10:33:41 -0600

    cleanup

--- Sample Projects/Rooms/Rooms/ContentView.swift
+++ Sample Projects/Rooms/Rooms/ContentView.swift
@@ -6,16 +6,19 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List(rooms) { room in
-                Image(room.name)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .cornerRadius(8)
-                VStack(alignment: .leading) {
-                    Text(room.name)
-                    Text("\(room.capacity) people")
-                        .font(.subheadline)
-                        .foregroundColor(.secondary)
-                }
+                NavigationLink(
+                    destination: Text(room.name)) {
+                        Image(room.name)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(8)
+                        VStack(alignment: .leading) {
+                            Text(room.name)
+                            Text("\(room.capacity) people")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                        }
+                    }
             }.navigationBarTitle(Text("Rooms"))
         }
     }
