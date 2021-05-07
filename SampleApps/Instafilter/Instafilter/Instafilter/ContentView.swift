0a5de5bc5d792e272d4c02e37af68d881d4a2d1b
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-05-07 15:03:09 -0600

    cleanup

--- SampleApps/Instafilter/Instafilter/Instafilter/ContentView.swift
+++ SampleApps/Instafilter/Instafilter/Instafilter/ContentView.swift
@@ -8,28 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount: CGFloat = 0
+    @State private var showingActionSheet = false
+    @State private var backgroundColor = Color.white
 
     var body: some View {
-        let blur = Binding<CGFloat>(
-            get: {
-                self.blurAmount
-            },
-            set: {
-                self.blurAmount = $0
-                print("New value is \(self.blurAmount)")
+        Text("Hello, World!")
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+            .onTapGesture {
+                self.showingActionSheet = true
             }
-        )
-
-        return VStack {
-            Text("Hello, World!")
-                .blur(radius: blurAmount)
-
-            Slider(value: blur, in: 0...20)
-        }
+            .actionSheet(isPresented: $showingActionSheet) {
+                ActionSheet(title: Text("Change background"), message: Text("Select a new color"), buttons: [
+                    .default(Text("Red")) { self.backgroundColor = .red },
+                    .default(Text("Green")) { self.backgroundColor = .green },
+                    .default(Text("Blue")) { self.backgroundColor = .blue },
+                    .cancel()
+                ])
+            }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
