f35ecf24735a56cdcd879aa941545813001c5e2c
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-05-07 05:26:55 -0600

    cleanup

---
+++ SampleApps/Moonshot/Moonshot/Moonshot/AstronautView.swift
@@ -0,0 +1,37 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by jrasmusson on 2021-05-07.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView(.vertical) {
+                VStack {
+                    Image(self.astronaut.id)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geometry.size.width)
+
+                    Text(self.astronaut.description)
+                        .padding()
+                }
+            }
+        }
+        .navigationBarTitle(Text(astronaut.name), displayMode: .inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+
+    static var previews: some View {
+        AstronautView(astronaut: astronauts[0])
+    }
+}
--- SampleApps/Moonshot/Moonshot/Moonshot/MissionView.swift
+++ SampleApps/Moonshot/Moonshot/Moonshot/MissionView.swift
@@ -32,23 +32,27 @@ struct MissionView: View {
                         .padding()
 
                     ForEach(self.astronauts, id: \.role) { crewMember in
-                        HStack {
-                            Image(crewMember.astronaut.id)
-                                .resizable()
-                                .frame(width: 83, height: 60)
-                                .clipShape(Capsule())
-                                .overlay(Capsule().stroke(Color.primary, lineWidth: 1))
-
-                            VStack(alignment: .leading) {
-                                Text(crewMember.astronaut.name)
-                                    .font(.headline)
-                                Text(crewMember.role)
-                                    .foregroundColor(.secondary)
+                        NavigationLink(destination: AstronautView(astronaut: crewMember.astronaut)) {
+                            
+                            HStack {
+                                Image(crewMember.astronaut.id)
+                                    .resizable()
+                                    .frame(width: 83, height: 60)
+                                    .clipShape(Capsule())
+                                    .overlay(Capsule().stroke(Color.primary, lineWidth: 1))
+                                
+                                VStack(alignment: .leading) {
+                                    Text(crewMember.astronaut.name)
+                                        .font(.headline)
+                                    Text(crewMember.role)
+                                        .foregroundColor(.secondary)
+                                }
+                                
+                                Spacer()
                             }
-
-                            Spacer()
+                            .padding(.horizontal)
                         }
-                        .padding(.horizontal)
+                        .buttonStyle(PlainButtonStyle())
                     }
                     
                     Spacer(minLength: 25)
