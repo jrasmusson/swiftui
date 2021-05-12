b6fa242f84efea9abb68e11a0a953d06cba7a8e8
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-05-12 06:56:23 -0600

    cleanup

--- Combine/TCDemo/TCDemo/TCDemo/ViewController.swift
+++ Combine/TCDemo/TCDemo/TCDemo/ViewController.swift
@@ -7,20 +7,43 @@
 
 import UIKit
 import SwiftUI
+import Combine
 
 final class ViewController: UIViewController {
 
     let termsLabel = UILabel()
-    let privacyLabel = UILabel()
-    
     let termsSwitch = UISwitch()
+
+    let privacyLabel = UILabel()
     let privacySwitch = UISwitch()
     
     let nameField = UITextField()
     let submitButton = UIButton()
     
+    // Define publishers
+    @Published private var acceptedTerms = false
+    @Published private var acceptedPrivacy = false
+    @Published private var name = ""
+    
+    // Define operators on publishers
+    private var validToSubmit: AnyPublisher<Bool, Never> {
+      return Publishers.CombineLatest3($acceptedTerms, $acceptedPrivacy, $name)
+        .map { terms, privacy, name in
+          terms && privacy && !name.isEmpty
+        }.eraseToAnyPublisher()
+    }
+    
+    // Define subscriptions
+    var stream: AnyCancellable?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Connect subsciber to publisher and assign to control via Keypath
+        stream = validToSubmit
+          .receive(on: RunLoop.main)
+          .assign(to: \.isEnabled, on: submitButton)
+        
         setup()
         style()
         layout()
@@ -35,21 +58,80 @@ extension ViewController {
     func style() {
         termsLabel.translatesAutoresizingMaskIntoConstraints = false
         termsLabel.text = "I accept the terms and conditions"
+        termsLabel.font = UIFont.preferredFont(forTextStyle: .subheadline)
         
         termsSwitch.translatesAutoresizingMaskIntoConstraints = false
+        termsSwitch.addTarget(self, action: #selector(acceptedTerms(_:)), for: .primaryActionTriggered)
         
+        privacyLabel.translatesAutoresizingMaskIntoConstraints = false
+        privacyLabel.text = "I accept the privacy policy"
+        privacyLabel.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        
+        privacySwitch.translatesAutoresizingMaskIntoConstraints = false
+        privacySwitch.addTarget(self, action: #selector(acceptedPrivacy(_:)), for: .primaryActionTriggered)
+        
+        nameField.translatesAutoresizingMaskIntoConstraints = false
+        nameField.placeholder = "Name"
+        nameField.borderStyle = .roundedRect
+        nameField.addTarget(self, action: #selector(nameChanged(_:)), for: .editingChanged)
+        
+        submitButton.translatesAutoresizingMaskIntoConstraints = false
+        submitButton.setTitle("Submit", for: .normal)
+        submitButton.backgroundColor = .systemBlue
+        submitButton.addTarget(self, action: #selector(submitPressed(_:)), for: .primaryActionTriggered)
     }
     
     func layout() {
         view.addSubview(termsLabel)
         view.addSubview(termsSwitch)
-        
+        view.addSubview(privacyLabel)
+        view.addSubview(privacySwitch)
+        view.addSubview(nameField)
+        view.addSubview(submitButton)
+
         NSLayoutConstraint.activate([
-            termsLabel.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 1),
-            termsLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: view.safeAreaLayoutGuide.leadingAnchor, multiplier: 1),
+            // Terms
+            termsLabel.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 3),
+            termsLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: view.safeAreaLayoutGuide.leadingAnchor, multiplier: 2),
             termsSwitch.centerYAnchor.constraint(equalTo: termsLabel.centerYAnchor),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: termsSwitch.trailingAnchor, multiplier: 1)
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: termsSwitch.trailingAnchor, multiplier: 2),
+            
+            // Privacy
+            termsSwitch.leadingAnchor.constraint(equalToSystemSpacingAfter: termsLabel.trailingAnchor, multiplier: 1),
+            privacyLabel.topAnchor.constraint(equalToSystemSpacingBelow: termsLabel.bottomAnchor, multiplier: 3),
+            privacyLabel.leadingAnchor.constraint(equalTo: termsLabel.leadingAnchor),
+            privacySwitch.centerYAnchor.constraint(equalTo: privacyLabel.centerYAnchor),
+            privacySwitch.trailingAnchor.constraint(equalTo: termsSwitch.trailingAnchor),
+            
+            // Name
+            nameField.topAnchor.constraint(equalToSystemSpacingBelow: privacyLabel.bottomAnchor, multiplier: 3),
+            nameField.leadingAnchor.constraint(equalTo: termsLabel.leadingAnchor),
+            nameField.trailingAnchor.constraint(equalTo: termsSwitch.trailingAnchor),
+            
+            // Button
+            submitButton.topAnchor.constraint(equalToSystemSpacingBelow: nameField.bottomAnchor, multiplier: 3),
+            submitButton.leadingAnchor.constraint(equalTo: termsLabel.leadingAnchor),
+            submitButton.trailingAnchor.constraint(equalTo: termsSwitch.trailingAnchor),
         ])
+    }
+}
+
+// MARK: - Actions
+extension ViewController {
+    @objc func acceptedTerms(_ sender: UISwitch) {
+        acceptedTerms = sender.isOn
+    }
+    
+    @objc func acceptedPrivacy(_ sender: UISwitch) {
+        acceptedPrivacy = sender.isOn
+    }
+    
+    @objc func nameChanged(_ sender: UITextField) {
+        name = sender.text ?? ""
+    }
+    
+    @objc func submitPressed(_ sender: UIButton) {
+        print("submitPressed!")
     }
 }
 
