67f3c44f78b4e2192899016f42f660f6efaf4640
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-05-21 13:47:41 -0600

    cleanup

---
+++ Combine/GettingStarted/GettingStarted/GettingStarted/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  GettingStarted
+//
+//  Created by jrasmusson on 2021-05-21.
+//
+
+import UIKit
+import Combine
+
+extension Notification.Name {
+    static let newBlogPost = Notification.Name("newPost")
+}
+
+struct BlogPost {
+    let title: String
+}
+
+class ViewController: UIViewController {
+
+    @IBOutlet var blogTextField: UITextField!
+    @IBOutlet var publishButton: UIButton!
+    @IBOutlet var subscribedLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        publishButton.addTarget(self, action: #selector(publishButtonTapped), for: .primaryActionTriggered)
+        
+        // Create a publisher
+        let publisher = NotificationCenter.Publisher(center: .default, name: .newBlogPost, object: nil)
+         .map { (notification) -> String? in
+             return (notification.object as? BlogPost)?.title ?? ""
+         }
+        
+        // Create a subscriber
+        let subscriber = Subscribers.Assign(object: subscribedLabel, keyPath: \.text)
+        publisher.subscribe(subscriber)
+    }
+    
+    @objc func publishButtonTapped(_ sender: UIButton) {
+        // Post the notification
+        let title = blogTextField.text ?? "Coming soon"
+        let blogPost = BlogPost(title: title)
+        NotificationCenter.default.post(name: .newBlogPost, object: blogPost)
+    }
+}
