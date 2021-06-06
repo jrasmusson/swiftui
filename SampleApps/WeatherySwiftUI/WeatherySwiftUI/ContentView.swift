ecff2231d86fd9f57f7352e17a56d221334dbb49
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-06-06 08:00:29 -0600

    cleanup

--- SampleApps/WeatherySwiftUI/WeatherySwiftUI/ContentView.swift
+++ SampleApps/WeatherySwiftUI/WeatherySwiftUI/ContentView.swift
@@ -8,144 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            EventHeader()
-            ImagePlaceholder()
-                .layoutPriority(-1)
-                .frame(minHeight: 100)
-            Text(makeDescription())
-            Spacer()
-            EventInfoList().fixedSize(horizontal: false, vertical: true)
-        }.padding()
-    }
-}
-
-struct ImagePlaceholder: View {
-    var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 10).stroke()
-            Text("Image placeholder")
-        }
-    }
-}
-
-struct EventInfoList: View {
-    var body: some View {
-        HeightSyncedRow {
-            EventInfoBadge(
-                iconName: "video.circle.fill",
-                text: "Video call available"
-            )
-            EventInfoBadge(
-                iconName: "doc.text.fill",
-                text: "Files are attached"
-            )
-            EventInfoBadge(
-                iconName: "person.crop.circle.badge.plus",
-                text: "Invites enabled, 5 people maximum"
-            )
-        }
-    }
-}
-
-struct EventInfoBadge: View {
-    var iconName: String
-    var text: String
-
-    var body: some View {
-        VStack {
-            Image(systemName: iconName)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 25, height: 25)
-            Text(text)
-                .frame(maxWidth: .infinity)
-                .multilineTextAlignment(.center)
-        }
-        .padding(.vertical, 10)
-        .padding(.horizontal, 5)
-        .background(Color.secondary)
-        .cornerRadius(10)
-    }
-}
-
-struct EventHeader: View {
-    var body: some View {
-        HStack(spacing: 15) {
-            CalendarView()
-            VStack(alignment: .leading) {
-                Text("Event title").font(.title)
-                Text("Location")
-            }
-            Spacer()
-        }
-    }
-}
-
-struct CalendarView: View {
-    var eventIsVerified = true
-
-    var body: some View {
-        Image(systemName: "calendar")
-            .resizable()
-            .frame(width: 50, height: 50)
-            .padding()
-            .background(Color.red)
-            .cornerRadius(10)
-            .foregroundColor(.white)
-            .addVerifiedBadge(eventIsVerified)
-    }
-}
-
-struct HeightSyncedRow<Content: View>: View {
-    private let content: Content
-    @State private var childHeight: CGFloat?
-
-    init(@ViewBuilder content: () -> Content) {
-        self.content = content()
-    }
-
+    @State private var cityName = ""
+    
     var body: some View {
         HStack {
-            content.frame(height: childHeight)
+            Image(systemName: "location.circle.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 44, height: 44)
+                
+            TextField("Search", text: $cityName)
+                .font(.title)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.asciiCapable)
+                .background(Color(.systemFill))
+            Image(systemName: "magnifyingglass")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 44, height: 44)
         }
-    }
-}
-
-extension View {
-    func addVerifiedBadge(_ isVerified: Bool) -> some View {
-        ZStack(alignment: .topTrailing) {
-            self
-
-            if isVerified {
-                Image(systemName: "checkmark.circle.fill")
-                    .alignAsBadge()
-            }
-        }
-    }
-}
-
-extension View {
-    func alignAsBadge(withRatio ratio: CGFloat = 0.8,
-                      alignment: Alignment = .topTrailing) -> some View {
-        alignmentGuide(alignment.horizontal) {
-            $0.width * ratio
-        }
-        .alignmentGuide(alignment.vertical) {
-            $0[.bottom] - $0.height * ratio
-        }
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-private extension ContentView {
-    func makeDescription() -> String {
-        String(repeating: "This is a description ", count: 50)
     }
 }
