400cfe4ec204190cffd0bbbcf18417c6b01675b9
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-06-06 11:38:06 -0600

    cleanup

--- SampleApps/WeatherySwiftUI/WeatherySwiftUI/ContentView.swift
+++ SampleApps/WeatherySwiftUI/WeatherySwiftUI/ContentView.swift
@@ -11,20 +11,23 @@ struct ContentView: View {
     @State private var cityName = ""
     
     var body: some View {
+        SearchView(cityName: $cityName)
+    }
+}
+
+struct SearchView: View {
+    @Binding var cityName: String
+    
+    var body: some View {
         HStack {
             Image(systemName: "location.circle.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 44, height: 44)
-                
+                .iconable()
             TextField("Search", text: $cityName)
                 .font(.title)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .keyboardType(.asciiCapable)
             Image(systemName: "magnifyingglass")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 44, height: 44)
+                .iconable()
         }
         .padding()
     }
@@ -33,5 +36,13 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension Image {
+    func iconable() -> some View {
+        self.resizable()
+        .aspectRatio(contentMode: .fit)
+        .frame(width: 44, height: 44)
     }
 }
