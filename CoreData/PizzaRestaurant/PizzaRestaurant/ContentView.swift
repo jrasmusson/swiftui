42242003b4a52d76822e8a96d74837038e1158d4
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-07-13 16:12:46 -0600

    cleanup

--- CoreData/PizzaRestaurant/PizzaRestaurant/ContentView.swift
+++ CoreData/PizzaRestaurant/PizzaRestaurant/ContentView.swift
@@ -10,13 +10,45 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-    @State var showOrderSheet = false
+    
+    @FetchRequest(entity: Order.entity(), sortDescriptors: [], predicate: NSPredicate(format: "status != %@", Status.completed.rawValue))
+    private var orders: FetchedResults<Order>
+    
+    @State private var showOrderSheet = false
     
     var body: some View {
         NavigationView {
             List {
-                Text("Sample order")
+                ForEach(orders) { order in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("\(order.pizzaType) - \(order.numberOfSlices) slices")
+                                .font(.headline)
+                            Text("Table \(order.tableNumber)")
+                                .font(.subheadline)
+                        }
+                        Spacer()
+                        Button(action: {
+                            updateOrder(order: order)
+                        }) {
+                            Text(order.orderStatus == .pending ? "Prepare" : "Complete")
+                                .foregroundColor(.blue)
+                        }
+                    }
+                    .frame(height: 50)
+                }
+                .onDelete { indexSet in
+                    for index in indexSet {
+                        viewContext.delete(orders[index])
+                    }
+                    do {
+                        try viewContext.save()
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }
             }
+            .listStyle(.plain)
             .navigationTitle("My Orders")
             .navigationBarItems(trailing: Button(action: {
                 showOrderSheet = true
@@ -29,6 +61,15 @@ struct ContentView: View {
             }
         }
     }
+    
+    func updateOrder(order: Order) {
+        let newStatus = order.orderStatus == .pending ? Status.preparing : .completed
+        viewContext.performAndWait {
+            order.orderStatus = newStatus
+            try? viewContext.save()
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
--- CoreData/PizzaRestaurant/PizzaRestaurant/OrderSheet.swift
+++ CoreData/PizzaRestaurant/PizzaRestaurant/OrderSheet.swift
@@ -8,11 +8,58 @@
 import SwiftUI
 
 struct OrderSheet: View {
+       
+    @Environment(\.managedObjectContext) private var viewContext
+    @Environment (\.presentationMode) var presentationMode
+    
+    let pizzaTypes = ["Pizza Margherita", "Greek Pizza", "Pizza Supreme", "Pizza California", "New York Pizza"]
+    
+    @State var selectedPizzaIndex = 1
+    @State var numberOfSlices = 1
+    @State var tableNumber = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section(header: Text("Pizza Details")) {
+                    Picker(selection: $selectedPizzaIndex, label: Text("Pizza Type")) {
+                        ForEach(0 ..< pizzaTypes.count) {
+                                Text(self.pizzaTypes[$0]).tag($0)
+                        }
+                    }
+                    
+                    Stepper("\(numberOfSlices) Slices", value: $numberOfSlices, in: 1...12)
+                }
+                
+                Section(header: Text("Table")) {
+                    TextField("Table Number", text: $tableNumber)
+                        .keyboardType(.numberPad)
+                    
+                }
+                
+                Button(action: {
+                    guard self.tableNumber != "" else {return}
+                    let newOrder = Order(context: viewContext)
+                    newOrder.pizzaType = self.pizzaTypes[self.selectedPizzaIndex]
+                    newOrder.orderStatus = .pending
+                    newOrder.tableNumber = self.tableNumber
+                    newOrder.numberOfSlices = Int16(self.numberOfSlices)
+                    newOrder.id = UUID()
+                    do {
+                        try viewContext.save()
+                        print("Order saved.")
+                        presentationMode.wrappedValue.dismiss()
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                }) {
+                    Text("Add Order")
+                }
+            }
+                .navigationTitle("Add Order")
+        }
     }
 }
-
 struct OrderSheet_Previews: PreviewProvider {
     static var previews: some View {
         OrderSheet()
