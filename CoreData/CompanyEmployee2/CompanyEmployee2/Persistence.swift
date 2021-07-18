331c4a30047cf7b318c70b72a66c3e4872a615ff
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-07-18 09:27:33 -0600

    cleanup

--- CoreData/CompanyEmployee2/CompanyEmployee2/Persistence.swift
+++ CoreData/CompanyEmployee2/CompanyEmployee2/Persistence.swift
@@ -8,13 +8,15 @@
 import CoreData
 
 struct PersistenceController {
+    let container: NSPersistentContainer
+    
     static let shared = PersistenceController()
     
     // Convenience
     var viewContext: NSManagedObjectContext {
         return container.viewContext
     }
-
+    
     static var preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
@@ -23,12 +25,11 @@ struct PersistenceController {
             newCompany.name = "Apple"
         }
         shared.saveContext()
-
+        
         return result
     }()
-
-    let container: NSPersistentContainer
-
+    
+    
     init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "CompanyEmployee2")
         if inMemory {
@@ -36,32 +37,21 @@ struct PersistenceController {
         }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                Typical reasons for an error here include:
-                * The parent directory does not exist, cannot be created, or disallows writing.
-                * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                * The device is out of space.
-                * The store could not be migrated to the current model version.
-                Check the error message to determine what the actual problem was.
-                */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
     }
     
     // Better save
-        func saveContext() {
-            let context = container.viewContext
-
-            if context.hasChanges {
-                do {
-                    try context.save()
-                } catch {
-                    fatalError("Error: \(error.localizedDescription)")
-                }
+    func saveContext() {
+        let context = container.viewContext
+        
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                fatalError("Error: \(error.localizedDescription)")
             }
         }
+    }
 }
