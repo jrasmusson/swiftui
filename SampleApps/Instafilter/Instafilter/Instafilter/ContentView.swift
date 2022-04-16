69f0654820c4fdaf13f188c9031c2432f06720ab
Jonathan Rasmusson <rasmus4200@gmail.com>
2022-04-16 12:48:27 -0600

    cleanup

---
+++ SampleApps/Instafilter/Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by jrasmusson on 2022-04-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
+
+    var body: some View {
+        Text("Hello, World!")
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+            .onTapGesture {
+                showingConfirmation = true
+            }
+            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+                Button("Red") { backgroundColor = .red }
+                Button("Green") { backgroundColor = .green }
+                Button("Blue") { backgroundColor = .blue }
+                Button("Cancel", role: .cancel) { }
+            } message: {
+                Text("Select a new color")
+            }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleApps/Instafilter/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by jrasmusson on 2022-04-16.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
