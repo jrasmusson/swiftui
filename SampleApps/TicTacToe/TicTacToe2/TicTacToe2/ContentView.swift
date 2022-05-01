6e007d6b62aa780aad83a34328792388a7ba3116
Jonathan Rasmusson <rasmus4200@gmail.com>
2022-05-01 07:38:43 -0600

    cleanup

--- SampleApps/TicTacToe/TicTacToe2/TicTacToe2/ContentView.swift
+++ SampleApps/TicTacToe/TicTacToe2/TicTacToe2/ContentView.swift
@@ -7,60 +7,6 @@
 
 import SwiftUI
 
-enum Value: CustomStringConvertible {
-    case b, x, o
-
-    var description: String {
-        switch self {
-        case .b:
-            return "-"
-        case .x:
-            return "X"
-        case .o:
-            return "O"
-        }
-    }
-}
-
-enum Position {
-    case upperLeft
-    case upperMiddle
-    case upperRight
-    case middleLeft
-    case middleMiddle
-    case middleRight
-    case lowerLeft
-    case lowerMiddle
-    case lowerRight
-}
-
-struct TileState {
-    var value: Value
-    var isLocked: Bool = false
-    var isX: Bool { value == .x }
-
-    static func blank() -> TileState {
-        TileState(value: .b)
-    }
-}
-
-enum GameOver: CustomStringConvertible {
-    case xWins
-    case oWins
-    case draw
-
-    var description: String {
-        switch self {
-        case .xWins:
-            return "X wins!"
-        case .oWins:
-            return "O wins!"
-        case .draw:
-            return "Draw!"
-        }
-    }
-}
-
 class ViewModel: ObservableObject {
     @Published var tileState = TileState(value: .b)
     @Published var isXTurn = false
