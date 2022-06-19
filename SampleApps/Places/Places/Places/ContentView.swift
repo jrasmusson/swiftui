47b356d70fff7dc638dc848b361160a3d668ecd7
Jonathan Rasmusson <rasmus4200@gmail.com>
2022-06-19 06:35:20 -0600

    cleanup

--- SampleApps/Places/Places/Places/ContentView.swift
+++ SampleApps/Places/Places/Places/ContentView.swift
@@ -7,23 +7,49 @@
 
 import SwiftUI
 
+enum Tab {
+    case places
+    case itineraries
+    case profile
+}
+
 struct ContentView: View {
     @EnvironmentObject var modelData: ModelData
+    @State var selection: Tab = .places
 
     var body: some View {
-        NavigationStack {
-            ScrollView {
-                ForEach(modelData.places) { place in
-                    NavigationLink(value: place) {
-                        PlaceCard(place: place)
+        TabView(selection: $selection) {
+            NavigationStack {
+                ScrollView {
+                    ForEach(modelData.places) { place in
+                        NavigationLink(value: place) {
+                            PlaceCard(place: place)
+                        }
                     }
                 }
+                .navigationTitle("Places")
+                .navigationDestination(for: Place.self) { item in
+                    PlaceDetail(place: place)
+                }
             }
-            .navigationTitle("Places")
-            .navigationDestination(for: Place.self) { item in
-                PlaceDetail(place: place)
+            .tabItem {
+                Label("Places", systemImage: "rectangle.fill.on.rectangle.angled.fill")
             }
+            .tag(Tab.places)
+
+            Text("Itineraries")
+                .tabItem {
+                    Label("Itineraries", systemImage: "list.bullet")
+                }
+                .tag(Tab.itineraries)
+
+            Text("Profile")
+                .tabItem {
+                    Label("Profile", systemImage: "person.fill")
+                }
+                .tag(Tab.profile)
         }
+
     }
 }
 
--- SampleApps/Places/README.md
+++ SampleApps/Places/README.md
@@ -148,3 +148,67 @@ struct PlaceDetail_Previews: PreviewProvider {
     }
 }
 ```
+
+## With Tab View
+
+![](images/2.png)
+
+**ContentView**
+
+```swift
+import SwiftUI
+
+enum Tab {
+    case places
+    case itineraries
+    case profile
+}
+
+struct ContentView: View {
+    @EnvironmentObject var modelData: ModelData
+    @State var selection: Tab = .places
+
+    var body: some View {
+        TabView(selection: $selection) {
+            NavigationStack {
+                ScrollView {
+                    ForEach(modelData.places) { place in
+                        NavigationLink(value: place) {
+                            PlaceCard(place: place)
+                        }
+                    }
+                }
+                .navigationTitle("Places")
+                .navigationDestination(for: Place.self) { item in
+                    PlaceDetail(place: place)
+                }
+            }
+            .tabItem {
+                Label("Places", systemImage: "rectangle.fill.on.rectangle.angled.fill")
+            }
+            .tag(Tab.places)
+
+            Text("Itineraries")
+                .tabItem {
+                    Label("Itineraries", systemImage: "list.bullet")
+                }
+                .tag(Tab.itineraries)
+
+            Text("Profile")
+                .tabItem {
+                    Label("Profile", systemImage: "person.fill")
+                }
+                .tag(Tab.profile)
+        }
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+            .environmentObject(ModelData())
+    }
+}
+```
