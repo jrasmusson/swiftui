c41ed46894d0791699eb2d8ea952c7f0b51d978d
Jonathan Rasmusson <rasmus4200@gmail.com>
2022-06-20 06:29:54 -0600

    cleanup

--- SampleApps/Places/Places/Places/NoteworthyRow.swift
+++ SampleApps/Places/Places/Places/NoteworthyRow.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct NoteworthyRow: View {
+    let noteworthy: Noteworthy
+    let size: CGFloat = 48
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(systemName: noteworthy.iconName)
+                .tint(.green)
+//                .resizable()
+//                .scaledToFit()
+//                .padding(10)
+
+
+                .frame(width: size, height: size)
+                .background(.secondary)
+                .cornerRadius(10)
+
+            VStack(alignment: .leading) {
+                Text(noteworthy.title)
+                    .font(.headline)
+                Text(noteworthy.description)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+        }
+        .padding()
+        .background(.ultraThinMaterial)
+        .cornerRadius(10)
     }
 }
 
 struct NoteworthyRow_Previews: PreviewProvider {
     static var previews: some View {
-        NoteworthyRow()
+        NoteworthyRow(noteworthy: notworthy1)
+            .preferredColorScheme(.dark)
     }
 }
