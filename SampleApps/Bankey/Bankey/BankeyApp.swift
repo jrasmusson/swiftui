99ec4ae08c2cf8045eb3167ee87fd4e0655f6d43
Jonathan Rasmusson <rasmus4200@gmail.com>
2022-06-22 05:40:02 -0600

    cleanup

---
+++ SampleApps/Bankey/Bankey/BankeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankeyApp.swift
+//  Bankey
+//
+//  Created by jrasmusson on 2022-06-22.
+//
+
+import SwiftUI
+
+@main
+struct BankeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleApps/Bankey/Bankey/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Bankey
+//
+//  Created by jrasmusson on 2022-06-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
