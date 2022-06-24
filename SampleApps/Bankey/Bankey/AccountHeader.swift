492b13e8d25f6984f66dc724e3f92f5861df381d
Jonathan Rasmusson <rasmus4200@gmail.com>
2022-06-24 06:08:39 -0600

    cleanup

---
+++ SampleApps/Bankey/Bankey/AccountHeader.swift
@@ -0,0 +1,32 @@
+//
+//  AccountHeader.swift
+//  Bankey
+//
+//  Created by jrasmusson on 2022-06-24.
+//
+
+import SwiftUI
+
+struct AccountHeader: View {
+    let size: CGFloat = 120
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("Good morning Kevin")
+                Text(Date.now.monthDayYearString)
+            }
+            Spacer()
+            Image(systemName: "sun.max.fill")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.yellow)
+                .frame(width: size, height: size)
+        }
+    }
+}
+
+struct AccountHeader_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountHeader()
+    }
+}
---
+++ SampleApps/Bankey/Bankey/Date+Ext.swift
@@ -0,0 +1,22 @@
+//
+//  Date+Ext.swift
+//  Bankey
+//
+//  Created by jrasmusson on 2022-06-24.
+//
+
+import Foundation
+
+extension Date {
+    static var bankeyDateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(abbreviation: "MDT")
+        return formatter
+    }
+
+    var monthDayYearString: String {
+        let dateFormatter = Date.bankeyDateFormatter
+        dateFormatter.dateFormat = "MMM d, yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
