52e404dd4aab6cdddde80f4d38d5de6a5f96b6c5
Jonathan Rasmusson <rasmus4200@gmail.com>
2022-07-04 07:27:37 -0600

    cleanup

--- SampleApps/CompanyEmployee/CompanyEmployee/CompanyViewModel.swift
+++ SampleApps/CompanyEmployee/CompanyEmployee/CompanyViewModel.swift
@@ -11,9 +11,6 @@ struct Employee: Codable, Identifiable, Hashable {
     let name: String
 }
 
-///
-/// Fake data
-///
 let employee1 = Employee(id: "1", name: "Jobs")
 let employee2 = Employee(id: "2", name: "Watson")
 let employee3 = Employee(id: "3", name: "Gates")
@@ -26,12 +23,31 @@ let company3 = Company(id: "3", name: "Microsoft", employees: employees)
 @MainActor
 class CompanyViewModel: ObservableObject {
 
-    /// Fake data
-    // @Published var items: [Company] = [company1, company2, company3]
     @Published var companies: [Company] = []
     @Published var showingError = false
 
-    /// Real thing
+    func fetchCompanies() async {
+        let fetchTask = Task { () -> [Company] in
+            let url = URL(string: "https://fierce-retreat-36855.herokuapp.com/company")!
+            let (data, _) = try await URLSession.shared.data(from: url) // (data, response)
+            let companies = try JSONDecoder().decode([Company].self, from: data)
+            return companies
+        }
+
+        let result = await fetchTask.result
+
+        switch result {
+            case .success(let companies):
+            self.companies = companies
+            self.showingError = false
+            case .failure(let error):
+            self.showingError = true
+        }
+    }
+}
+
+
+/// Real thing
 //    func fetchCompanies() async {
 //        guard let url = URL(string: "https://fierce-retreat-36855.herokuapp.com/company") else { fatalError("Missing URL") }
 //
@@ -60,26 +76,3 @@ class CompanyViewModel: ObservableObject {
 //
 //        dataTask.resume()
 //    }
-
-    /// Real thing with `Result`
-    func fetchCompanies() async {
-        let fetchTask = Task { () -> [Company] in
-            let url = URL(string: "https://fierce-retreat-36855.herokuapp.com/company")!
-            let (data, _) = try await URLSession.shared.data(from: url) // (data, response)
-            let companies = try JSONDecoder().decode([Company].self, from: data)
-            return companies
-        }
-
-        // TODO: Handle response 200 set networkError
-
-        let result = await fetchTask.result
-
-        switch result {
-            case .success(let companies):
-            self.companies = companies
-            self.showingError = false
-            case .failure(let error):
-            self.showingError = true
-        }
-    }
-}
